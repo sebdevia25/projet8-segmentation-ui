3909681c1ccb6404822c609a2e6053244acf22c8
sebdevia25 <seb.devia25@gmail.com>
2026-03-13 13:51:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,6 +78,7 @@ if "selected_label" not in st.session_state:
     st.session_state.selected_label = "All"
 
 st.title("Cityscapes Segmentation Comparison")
+st.write(os.listdir(IMG_DIR))
 st.write(os.listdir(EDA_DIR))
 tab1, tab2 = st.tabs(["EDA", "MODELS"])
 
